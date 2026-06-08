44b803b0be4e6b30ebb8c85087071c3b6f4d9161
coldchainrescue <hanisahjohaari@gmail.com>
2026-06-08 10:37:17 +0800

    update api

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://preservelink-backend.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 });
 
